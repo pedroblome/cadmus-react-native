6849ae7ad65699a62e249d6d976c33ba8fb35e28
pedroblome <joaopedroblome78@gmail.com>
2026-04-11 22:43:20 -0300

    feat: configuração inicial do Expo Router e rotas para escolas e turmas

---
+++ prefeitura-escolas/app/classes/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function ClassesScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Lista de Turmas</Text>
+    </View>
+  );
+}
