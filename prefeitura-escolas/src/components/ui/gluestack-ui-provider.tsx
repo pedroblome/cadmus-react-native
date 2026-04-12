e07840b7e55dc5fa6948c8d881c5c7fa92802483
pedroblome <joaopedroblome78@gmail.com>
2026-04-11 22:56:07 -0300

    chore: estrutura de pastas baseada em features e domínios

--- prefeitura-escolas/src/components/ui/gluestack-ui-provider.tsx
+++ prefeitura-escolas/src/components/ui/gluestack-ui-provider.tsx
@@ -1,6 +1,6 @@
-import { GluestackUIProvider as Provider } from '@gluestack-ui/themed';
-import theme from '../../theme/gluestack-theme.config';
-import { ReactNode } from 'react';
+import { GluestackUIProvider as Provider } from "@gluestack-ui/themed";
+import theme from "../../theme/gluestack-theme.config";
+import { ReactNode } from "react";
 
 export function GluestackUIProvider({ children }: { children: ReactNode }) {
   return <Provider config={theme}>{children}</Provider>;
